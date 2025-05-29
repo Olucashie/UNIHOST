0d3bca1f5587f470883693ab49dc59c4fd1b72da
Oluwabori Testimony <olucashie@gmail.com>
2025-05-29 04:58:18 -0700

    feat: integrate Tailwind CSS and add Header component
    - Added Tailwind CSS and autoprefixer to dependencies.
    - Created postcss.config.js for Tailwind CSS configuration.
    - Implemented Header component with navigation links and search functionality.
    - Updated App component to include the new Header.
    - Modified tailwind.config.js to include content paths for Tailwind CSS.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/Signin';
 import Home from './pages/Home';
+import Header from './components/Header';
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/signin' element={<SignIn />} />
