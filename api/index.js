7175f3a5f1c3186bba1e2f04773190d27a5c00a0
Oluwabori Testimony <olucashie@gmail.com>
2025-05-29 08:55:51 -0700

    chore: initialize project with package.json and essential dependencies

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.uri)
+
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((err) => {
+    console.error(err);
+});
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000!!');
+});
