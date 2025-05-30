9594880934e0c0edd94c414da976e66ba4735bb2
Oluwabori Testimony <olucashie@gmail.com>
2025-05-30 04:25:26 -0700

    feat: add user and auth routes with corresponding controllers

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import  userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
-mongoose.connect(process.env.uri)
+mongoose.connect("mongodb+srv://olucashie:Testimony@cluster0.hhk1m4t.mongodb.net/UNIHOST?retryWrites=true&w=majority&appName=Cluster0")
 
 .then(() => {
     console.log('Connected to MongoDB');
@@ -17,3 +19,6 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000!!');
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
